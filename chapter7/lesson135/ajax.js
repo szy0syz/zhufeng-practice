93029ed92657545de611ebd8fc4eef7cfaa2fcce
szy0syz <szy0syz@gmail.com>
2017-07-25 15:56:31 +0800

    finished lesson135

---
+++ chapter7/lesson135/ajax.js
@@ -0,0 +1,30 @@
+function createXHR() {
+  var xhr = null,
+    ary = [
+      function () {
+        return new XMLHttpRequest;
+      },
+      function () {
+        return new ActiveXObject("Microsoft.XMLHTTP");
+      },
+      function () {
+        return new ActiveXObject("Msxml2.XMLHTTP");
+      },
+      function () {
+        return new ActiveXObject("Msxml3.XMLHTTP");
+      }
+    ];
+  var i, len;
+  for (i = 0, len = ary.length; i < len; i++) {
+    try {
+      xhr = ary[i];
+      // 如果能运行到下面说明刚好能兼容
+      // 既然能兼容就直接修改这个函数堆内存指向数组中兼容的函数
+      createXHR = ary[i];
+      break; //不能再循环了
+    } catch (e) {
+      console.log('Your bowser is not support ajax.');
+    }
+  }
+  return xhr;
+}
