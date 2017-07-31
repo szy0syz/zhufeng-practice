9d9bb7daf2039d3dc90fc4604ddc1e24ed2ba761
szy0syz <szy0syz@gmail.com>
2017-07-31 23:58:58 +0800

    updated lesson 157

--- chapter7/lesson157/serv.js
+++ chapter7/lesson157/serv.js
@@ -2,10 +2,58 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+String.prototype.queryURLParams = function () {
+  let obj = {},
+    reg = /([^?=&#]+)=([^?=&#]+)/g;
+  this.replace(reg, function () {
+    // arguments[0] === this
+    const key = arguments[1];
+    obj[key] = arguments[2];
+  });
+  return obj;
+};
+
 const serv = http.createServer(function (req, res) {
   const urlObj = url.parse(req.url);
   const pathname = urlObj.pathname;
   query = urlObj['query'];
+
+  // -> 处理静态资源的请求
+  const reg = /.(HTML|JS|CSS|ICO)/i; // 小正则捕获后缀名，不区分大小写。
+  if (reg.test(pathname)) {
+    const suffix = reg.exec(pathname)[1].toUpperCase();
+    const suffixMIME = suffix === 'HTML' ? 'text/html' : (suffix === 'CSS' ? 'text/css' : (suffix === 'JS' ? 'text/javascript' : (suffix === 'ICO' ? 'image/x-icon' : 'text/plain')));
+    try {
+      res.writeHead(200, {'content-type': `${suffixMIME};charset=utf-8;`});
+      res.end(fs.readFileSync('.' + pathname, 'utf-8'));
+    } catch (e) {
+      res.writeHead(404);
+      res.end('file is not found.');
+    }
+    return
+  }
+
+  query = query.queryURLParams();
+
+  // 处理API接口文档规定的数据请求
+  let data = JSON.parse(fs.readFileSync('./json/students.json', 'utf-8'));
+  if (pathname === '/getList') {
+    const n = query['n'];
+    let ary = [];
+    for (let i = (n - 1) * 3; i <= n * 3 - 1; i++) {
+      // 如果已经到了数组最后一个，直接跳出了，因为之前每次都push，不怕得。
+      if(i > data.length-1) {
+        break;
+      }
+      ary.push(data[i]);
+    }
+    res.writeHead(200, {'content-type': 'application/json;charset=utf-8;'});
+    res.end(JSON.stringify(ary));
+
+    return
+  }
+
+  res.end('404');
 });
 
 serv.listen(8001, function () {
