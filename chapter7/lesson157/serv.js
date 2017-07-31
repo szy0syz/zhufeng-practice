d3adaad5a2f0ad98efba967f001daf055dec5477
szy0syz <szy0syz@gmail.com>
2017-08-01 00:14:59 +0800

    lesson 157 后端完成

--- chapter7/lesson157/serv.js
+++ chapter7/lesson157/serv.js
@@ -17,7 +17,8 @@ const serv = http.createServer(function (req, res) {
   const urlObj = url.parse(req.url);
   const pathname = urlObj.pathname;
   query = urlObj['query'];
-
+  let i = 0,
+    result = null;
   // -> 处理静态资源的请求
   const reg = /.(HTML|JS|CSS|ICO)/i; // 小正则捕获后缀名，不区分大小写。
   if (reg.test(pathname)) {
@@ -40,19 +41,46 @@ const serv = http.createServer(function (req, res) {
   if (pathname === '/getList') {
     const n = query['n'];
     let ary = [];
-    for (let i = (n - 1) * 3; i <= n * 3 - 1; i++) {
+    for (i = (n - 1) * 3; i <= n * 3 - 1; i++) {
       // 如果已经到了数组最后一个，直接跳出了，因为之前每次都push，不怕得。
-      if(i > data.length-1) {
+      if (i > data.length - 1) {
         break;
       }
       ary.push(data[i]);
     }
     res.writeHead(200, {'content-type': 'application/json;charset=utf-8;'});
-    res.end(JSON.stringify(ary));
+    res.end(JSON.stringify({
+      code: 0,
+      msg: 'ok',
+      total: Math.ceil(data.length / 3),
+      data: ary
+    }));
 
     return
   }
 
+  if (pathname === '/getInfo') {
+    const sid = query['id'];
+    let obj = null;
+    for (i = 0; i < data.length; i++) {
+      if (data[i].id.toString() === sid) { // 劳资死也不用'=='，因为JS语言精粹的作者大叔和我说的！
+        obj = data[i];
+      }
+    }
+    result = { code: 1, msg: '内容不存在', data: null};
+    if (obj) {
+      result = {
+        code: 0,
+        msg: 'ok',
+        data: obj
+      }
+    }
+    res.writeHead(200, {'content-type': 'application/json;charset=utf-8;'});
+    res.end(JSON.stringify(result));
+    return
+  }
+
+  // 啥也没找到就404
   res.end('404');
 });
 
