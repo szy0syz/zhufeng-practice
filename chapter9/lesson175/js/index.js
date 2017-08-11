73a06044075d9a4b29f037226c5bdf88a3cafcc6
szy0syz <szy0syz@gmail.com>
2017-08-11 15:11:34 +0800

    updated lesson 176 header 终于完美！

--- chapter9/lesson175/js/index.js
+++ chapter9/lesson175/js/index.js
@@ -2,12 +2,15 @@ var navmain = document.getElementById('nav-main');
 var listA = utils.children(navmain,'a');
 
 var navHoverHandle = function navHoverHandle(e) {
+  // 注意：这里所触发事件的元素已限定是a标签
   e = e || window.event;
   e. target = e. target || e.srcElement;
   if (e.type === 'mouseenter') {
     utils.addClass(utils.children(e.target, 'i')[0], 'nav-hover');
+    e.target.style.opacity = 1;
   } else {
     utils.removeClass(utils.children(e.target, 'i')[0], 'nav-hover');
+    e.target.style.opacity = 0.8;
   }
 };
 
