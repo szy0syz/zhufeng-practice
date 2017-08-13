a9fff2d7e592777e432357e74b4927648e3191e6
szy0syz <szy0syz@gmail.com>
2017-08-13 22:06:34 +0800

    added 响应式布局和page1样式完

--- chapter9/lesson179/js/index.js
+++ chapter9/lesson179/js/index.js
@@ -1,7 +1,22 @@
+~function () {
+  var desW = 640, // 设计原始稿宽度
+    winW = document.documentElement.clientWidth,
+    main = document.getElementById('#main'),
+    ratio = winW / desW;
+
+  if (winW > desW) {
+    main.style.width = desw;
+    main.style.margin = '0 auto';
+    // document.documentElement.style.fontSize = ratio
+    return;
+  }
+
+  document.documentElement.style.fontSize = ratio * 100 + 'px';
+}();
+
 !function () {
   new Swiper('.swiper-container', {
     direction: 'vertical',
     loop: true
   });
-
 }();
